643584cec96c7bde3460583557e04df35e1435dc
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-04-19 17:20:10 +0200

    delete connection in main class

--- BookManager/src/main/java/com/imatia/bookmanager/BookManagerApp.java
+++ BookManager/src/main/java/com/imatia/bookmanager/BookManagerApp.java
@@ -1,12 +1,5 @@
 package com.imatia.bookmanager;
 
-<<<<<<< HEAD
-
-=======
-import java.sql.SQLException;
-
-import com.imatia.bookmanager.model.data.ConnectionSQLite;
->>>>>>> branch 'Development' of https://github.com/adrianpm99/biblioteca_ImatiaFCT
 
 /**
  * BookManagerApp
@@ -16,17 +9,6 @@ public class BookManagerApp
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-    	
-=======
-    	ConnectionSQLite connect = new ConnectionSQLite();
-    	try {
-			connect.getConnection();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
->>>>>>> branch 'Development' of https://github.com/adrianpm99/biblioteca_ImatiaFCT
+
     }
 }
