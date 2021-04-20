93a210105c7d81880b375a6e5648cd797e686825
Laura <Laura@DESKTOP-C5E0PRE>
2021-04-20 10:59:03 +0200

    .

--- BookManager/src/main/java/com/imatia/bookmanager/BookManagerApp.java
+++ BookManager/src/main/java/com/imatia/bookmanager/BookManagerApp.java
@@ -1,32 +1,15 @@
 package com.imatia.bookmanager;
 
-<<<<<<< HEAD
 
-=======
 import java.sql.SQLException;
 
 import com.imatia.bookmanager.model.data.ConnectionSQLite;
->>>>>>> branch 'Development' of https://github.com/adrianpm99/biblioteca_ImatiaFCT
 
-/**
- * BookManagerApp
- *
- */
 public class BookManagerApp 
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-    	
-=======
-    	ConnectionSQLite connect = new ConnectionSQLite();
-    	try {
-			connect.getConnection();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
->>>>>>> branch 'Development' of https://github.com/adrianpm99/biblioteca_ImatiaFCT
+
+
     }
 }
