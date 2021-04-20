c1aa2a42ddc705f8861d063e1df9d3c27853c087
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-04-20 11:30:14 +0200

    .

--- BookManager/src/main/java/com/imatia/bookmanager/BookManagerApp.java
+++ BookManager/src/main/java/com/imatia/bookmanager/BookManagerApp.java
@@ -4,17 +4,10 @@ package com.imatia.bookmanager;
  * BookManagerApp
  *
  */
-<<<<<<< HEAD
+
 public class BookManagerApp {
 	public static void main(String[] args) {
 		
 	}
-=======
-public class BookManagerApp 
-{
-    public static void main( String[] args )
-    {
-        
-    }
->>>>>>> refs/heads/Development
+
 }
