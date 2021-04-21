eb072e664cafe1b82b76088fa511c5a052748762
Laura <Laura@DESKTOP-C5E0PRE>
2021-04-21 19:00:02 +0200

    copylending

--- BookManager/src/main/java/com/imatia/bookmanager/view/menus/SearchesMenu.java
+++ BookManager/src/main/java/com/imatia/bookmanager/view/menus/SearchesMenu.java
@@ -3,10 +3,10 @@ package com.imatia.bookmanager.view.menus;
 import com.imatia.bookmanager.controller.BookController;
 import com.imatia.bookmanager.view.inputs.InputUserData;
 import com.imatia.bookmanager.view.results.BookRenderers;
-import com.imatia.bookmanager.view.ui.ResultsByMethodMenu;
-import com.imatia.bookmanager.view.ui.StartMenu;
+import com.imatia.bookmanager.view.ui.ResultsByMethodUi;
+import com.imatia.bookmanager.view.ui.StartMenuUI;
 
-public class SearchesMenuController {
+public class SearchesMenu {
 
 	public static void showSearchesMenuOptions() {
 		
@@ -19,31 +19,31 @@ public class SearchesMenuController {
 			switch (option) {
 
 			case 0:
-				StartMenu.showStartMenu();
+				StartMenuUI.showStartMenuUi();
 				break;
 			case 1:
 				System.out.print("Introduzca el autor: ");
 				filter = InputUserData.checkUserInput("author");
 				
-				ResultsByMethodMenu.showResultsView("AUTOR");
+				ResultsByMethodUi.showResultsUi("AUTOR");
 				BookRenderers.renderAuthorListBooks(bc.getBooksByAuthor(filter));
 				
-				ResultsByMethodMenuController.showResultsByMethodMenuOptions();	
+				ResultsByMethodMenu.showResultsByMethodMenuOptions();	
 				break;
 			case 2:
 				System.out.print("Introduzca el titulo: ");
 				filter = InputUserData.checkUserInput("title");
-				ResultsByMethodMenu.showResultsView("TITULO");
+				ResultsByMethodUi.showResultsUi("TITULO");
 				BookRenderers.renderTitleListBooks(bc.getBookByTitle(filter));
-				ResultsByMethodMenuController.showResultsByMethodMenuOptions();	
+				ResultsByMethodMenu.showResultsByMethodMenuOptions();	
 				
 				break;
 			case 3:
 				System.out.print("Introduzca el ISBN: ");
 				filter = InputUserData.checkUserInput("ISBN");
-				ResultsByMethodMenu.showResultsView("ISBN");
+				ResultsByMethodUi.showResultsUi("ISBN");
 				BookRenderers.renderISBNListBooks(bc.getBookByIsbn(filter));
-				ResultsByMethodMenuController.showResultsByMethodMenuOptions();	
+				ResultsByMethodMenu.showResultsByMethodMenuOptions();	
 				break;
 			}
 		} while (option != 0 && option != 1 && option != 2 && option != 3);
