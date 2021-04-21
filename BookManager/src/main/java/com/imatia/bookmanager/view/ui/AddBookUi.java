2f231cf07730a4d7bf2a55501372401762948094
AlbertoPerezCotos <albertoperez13@hotmail.com>
2021-04-21 16:58:26 +0200

    .

---
+++ BookManager/src/main/java/com/imatia/bookmanager/view/ui/AddBookUi.java
@@ -0,0 +1,22 @@
+package com.imatia.bookmanager.view.ui;
+
+import com.imatia.bookmanager.view.menus.AddBookMenu;
+
+/**
+ * This is the UI for the Add Book menu.
+ */
+public class AddBookUi
+{
+	public static void showAddBookUi()
+	{
+		System.out.println(
+				"\n******************\r\n" + 
+				"** AÑADIR LIBRO **\r\n" + 
+				"******************\r\n" + 
+				"------------------------------------------\r\n" + 
+				"|| Introduzca los datos del nuevo libro ||\r\n" + 
+				"------------------------------------------");
+		
+		AddBookMenu.showAddBookMenu();
+	}//showAddBookMenu()
+}//class AddBookMenu
