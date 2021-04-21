be053f20a83b0b8dd7775f78608e8a579fe77de8
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-04-21 20:00:39 +0200

    solve conflicts

--- BookManager/src/main/java/com/imatia/bookmanager/view/menus/BookDetailsMenu.java
+++ BookManager/src/main/java/com/imatia/bookmanager/view/menus/BookDetailsMenu.java
@@ -1,11 +1,9 @@
 package com.imatia.bookmanager.view.menus;
 
 import com.imatia.bookmanager.view.inputs.InputUserData;
-<<<<<<< HEAD
-import com.imatia.bookmanager.view.ui.AddBookUi;
 import com.imatia.bookmanager.view.ui.SearchesUi;
-=======
->>>>>>> refs/heads/feature/addMenu
+
+
 
 public class BookDetailsMenu
 {
@@ -26,10 +24,7 @@ public class BookDetailsMenu
 				//Not implemented yet
 			case 2:
 				//Not implemented yet
-<<<<<<< HEAD
-=======
 
->>>>>>> refs/heads/feature/addMenu
 			}
 
 		} while (option != 0 && option != 1 && option != 2);
