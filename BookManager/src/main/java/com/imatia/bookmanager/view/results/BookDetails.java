d75de6ce6d3f37ac797b212a892b0b63ca535c83
barrio <barrio@MSI-V7O>
2021-04-22 11:01:29 +0200

    .

--- BookManager/src/main/java/com/imatia/bookmanager/view/results/BookDetails.java
+++ BookManager/src/main/java/com/imatia/bookmanager/view/results/BookDetails.java
@@ -1,7 +1,6 @@
 package com.imatia.bookmanager.view.results;
 
 import com.imatia.bookmanager.model.entities.Book;
-import com.imatia.bookmanager.view.menus.BookDetailsMenu;
 
 /**
  * This view shows the information details
@@ -9,29 +8,26 @@ import com.imatia.bookmanager.view.menus.BookDetailsMenu;
  */
 public class BookDetails
 {
-	public static void showBookDetails(Book b)
+	public static void main(String[] args)
 	{
-	
+		Book b= new Book();
 		System.out.println(
-				"\n*********************\r\n" + 
+				"*********************\r\n" + 
 				"** DATOS DEL LIBRO **\r\n" + 
 				"*********************\r\n" + 
-				"-----------------------------------------------------------------------------\r\n" + 
-				"|| 1- Editar libro || 2- Eliminar libro || 0- Volver al menu de busquedas  ||\r\n" + 
-				"-----------------------------------------------------------------------------\r\n" + 
+				"----------------------------------------------------------\r\n" + 
+				"|| 1- Editar libro || 0- Volver al menu de resultados	||\r\n" + 
+				"----------------------------------------------------------\r\n" + 
 				"|| Titulo: "+b.getTitle()+"\r\n" + 
 				"||	\r\n" + 
 				"|| Autor: "+b.getAuthor()+"\r\n" + 
 				"||	\r\n" + 
-				"|| ISBN: "+b.getISBN()+"	|| Año: "+b.getBookPublicationYear()+ "	|| Edicion: "+b.getEdition()+"ª\r\n" + 
+				"|| ISBN: "+b.getISBN()+"	|| Año: "+b.getBookPublicationYear()+ "	|| Edicion: "+b.getEditorial()+"ª\r\n" + 
 				"|| \r\n" + 
-				"|| Editorial: "+b.getEditorial()+"	|| Hojas: "+b.getPageNumber()+"\r\n" + 
+				"|| Editorial: "+b.getEditorial()+"	|| Hojas: "+b.getNumberOfSheets()+"\r\n" + 
 				"==========================================================\r\n" + 
 				"Descripcion:\r\n"+ 
 				b.getDescription()+"\r\n" +
 				"==========================================================");
-		
-		BookDetailsMenu.showBookDetailsOptions(b.getId());
-
 	}//showBookDetails()
 }//class BookDetails
