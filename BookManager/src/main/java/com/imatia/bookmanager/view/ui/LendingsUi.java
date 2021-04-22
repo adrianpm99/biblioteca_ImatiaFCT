594f38dcfe4a81bd1ecd320a721515c4d132d4f2
barrio <barrio@MSI-V7O>
2021-04-22 13:05:36 +0200

    .

---
+++ BookManager/src/main/java/com/imatia/bookmanager/view/ui/LendingsUi.java
@@ -0,0 +1,21 @@
+package com.imatia.bookmanager.view.ui;
+
+/**
+ * Lendings UI
+ */
+public class LendingsUi
+{
+	public static void showLendingsUi()
+	{
+		System.out.println(
+				"***************\r\n" + 
+				"** PRESTAMOS **\r\n" + 
+				"***************\r\n" + 
+				"------------------------------------------------------------------------------------------------------\r\n" + 
+				"|| 1- Añadir prestamo || 2- Modificar prestamo || 3- Borrar prestamo || 0- Volver al menu principal ||\r\n" + 
+				"------------------------------------------------------------------------------------------------------");
+		
+		//Laura, te dejo la llamada al metodo de las opciones de menu. borra esto y descomenta lo de abajo
+		//LendingsMenu.showLendingsMenuOptions();
+	}//showLendingsUi()
+}//class LendingsUi
