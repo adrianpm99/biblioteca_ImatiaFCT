4978548565025dee478cbb12fdc569d64ed880d7
barrio <barrio@MSI-V7O>
2021-04-26 09:24:26 +0200

    Update user data inputs

--- BookManager/src/main/java/com/imatia/bookmanager/view/inputs/InputUserData.java
+++ BookManager/src/main/java/com/imatia/bookmanager/view/inputs/InputUserData.java
@@ -16,94 +16,96 @@ public class InputUserData {
 	 * @return "" if the inputUserData is not correct and return the inputString if
 	 *         its correct
 	 */
-	public static String checkUserInput(String dataType) {
-		String data = scan.nextLine();
+	public static String checkUserInput(String dataType, String errorSentence) {
 		Pattern pattern;
-		Matcher matcher;
-		switch (dataType) {
-		case "id":
-			pattern = Pattern.compile("^[\\d]{1,5}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "title":
-			pattern = Pattern.compile("^.{0,50}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "description":
-			pattern = Pattern.compile("^.{0,200}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "author":
-		case "editorial":
-			pattern = Pattern.compile("^[\\w\\s]{0,50}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "pageNumber":
-			pattern = Pattern.compile("^[\\d]{1,5}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "ISBN":
-			pattern = Pattern.compile("^[\\d]{13}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "edition":
-			pattern = Pattern.compile("^[\\d]{0,3}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "bookPublicationYear":
-			pattern = Pattern.compile("^[012]{1}+[\\d]{3}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "option":
-			pattern = Pattern.compile("^[\\d]$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
+        Matcher matcher;
+        String data;
+		do {
+            data = scan.nextLine();
+            switch (dataType) {
+            case "id":
+                pattern = Pattern.compile("^[\\d]{0,5}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "title":
+                pattern = Pattern.compile("^.{0,50}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "description":
+                pattern = Pattern.compile("^.{0,200}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "author":
+            case "editorial":
+                pattern = Pattern.compile("^.{0,50}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "pageNumber":
+                pattern = Pattern.compile("^[\\d]{0,5}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "ISBN":
+                pattern = Pattern.compile("^[\\d]{13}$|^.{0}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "edition":
+                pattern = Pattern.compile("^[\\d]{0,3}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "bookPublicationYear":
+                pattern = Pattern.compile("^[012]{1}+[\\d]{3}$|^.{0}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "copyNumber":
+                pattern = Pattern.compile("^[\\d]{0,3}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            case "option":
+                pattern = Pattern.compile("^[\\d]{1}$");
+                matcher = pattern.matcher(data);
+                if (matcher.find()) {
+                    return data;
+                }
+                break;
+            default:
+                return data;
+            }
+            System.out.println("\u001B[31m" + "Datos con el formato incorrecto." + "\u001B[0m");
+            if (!errorSentence.equals("")) {
+                System.out.println(errorSentence);
+            }
+        } while (data.equals(""));
+		return data;
+    }
 
-		case "copyNumber":
-			pattern = Pattern.compile("^[\\d]{0,3}$");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-			break;
-		case "0":
-			pattern = Pattern.compile("0");
-			matcher = pattern.matcher(data);
-			if (matcher.find()) {
-				return data;
-			}
-		default:
-			return data;
-		}
-		
-		System.out.println("\u001B[31m" + "Datos con el formato incorrecto." + "\u001B[0m");
-		return "";
-	}
+    public static String checkUserInput(String dataType) {
+        return checkUserInput(dataType, "");
+    }
 }
