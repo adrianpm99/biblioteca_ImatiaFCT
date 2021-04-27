c99a0fead453799bd8ae1433ffff8ca62ca845bb
barrio <barrio@MSI-V7O>
2021-04-27 17:15:06 +0200

    Added reservationUi

---
+++ BookManager/src/main/java/com/imatia/bookmanager/view/ui/AddReservationUi.java
@@ -0,0 +1,18 @@
+package com.imatia.bookmanager.view.ui;
+
+import com.imatia.bookmanager.view.menus.ReservationMenu;
+
+public class AddReservationUi {
+public static void showAddUserUi() {
+		
+		System.out.println(
+				"\n**************************\r\n" + 
+				"** AÑADIR NUEVA RESERVA **\r\n" + 
+				"**************************\r\n" + 
+				"--------------------------------------------\r\n" + 
+				"|| Introduzca los datos de la reserva ||\r\n" + 
+				"--------------------------------------------");
+		
+		//ReservationMenu.showAddUserMenu();
+	}
+}
