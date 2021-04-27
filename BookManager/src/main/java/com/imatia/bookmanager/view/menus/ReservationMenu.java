9b4cd2d3731bee1d4ceb4288046fc5f0c9a34363
barrio <barrio@MSI-V7O>
2021-04-27 16:41:38 +0200

    controller and menu reservation

---
+++ BookManager/src/main/java/com/imatia/bookmanager/view/menus/ReservationMenu.java
@@ -0,0 +1,28 @@
+package com.imatia.bookmanager.view.menus;
+
+import com.imatia.bookmanager.view.inputs.InputUserData;
+import com.imatia.bookmanager.view.ui.StartMenuUI;
+import com.imatia.bookmanager.controller.ReservationController;
+
+public class ReservationMenu {
+	public static void showReservationOptions() {
+		String option;
+		//Option
+		do {
+			option = InputUserData.checkUserInput("option", "Opcion no valida. Pruebe de nuevo (entero positivo)");
+		} while (option.equals("") && !option.equals("0") && !option.equals("1"));
+		
+		switch (Integer.parseInt(option))
+		{
+		case 0:
+			//Return to de start Menu
+			StartMenuUI.showStartMenuUi();
+			break;
+			
+		case 1:
+			//option to list by copy 
+			//ReservationRenderers(by Copy)
+			break;
+		}
+	}//showReservationMenuOptions()
+}//class ReservationsMenu
