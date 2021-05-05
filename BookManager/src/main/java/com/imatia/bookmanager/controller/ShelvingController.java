5938c7eb984b99bc89d3ee657f43a3f1dede2358
barrio <barrio@MSI-V7O>
2021-05-05 16:36:30 +0200

    Solving error merge

--- BookManager/src/main/java/com/imatia/bookmanager/controller/ShelvingController.java
+++ BookManager/src/main/java/com/imatia/bookmanager/controller/ShelvingController.java
@@ -3,7 +3,15 @@ package com.imatia.bookmanager.controller;
 import com.imatia.bookmanager.model.data.ShelvingDao;
 import com.imatia.bookmanager.model.entities.Shelving;
 
+/**
+ * class to implement the methods to connect views with data
+ * 
+ * @author Grupo2FCTImatia
+ *
+ */
 public class ShelvingController {
+	
+	ShelvingDao sd = new ShelvingDao();
 	
 	/**
 	 * Get a Shelving Object by copyId
@@ -12,12 +20,32 @@ public class ShelvingController {
 	 */
 	public  Shelving getShelvingByCopyId(int copyId) {	
 		
-		ShelvingDao sd = new ShelvingDao();
-		
-		Shelving shelving = sd.getShelvingByCopyId(copyId);
-		
-		return shelving;
+		return sd.getShelvingByCopyId(copyId);
 		
 	}//getShelvingByCopyId()
+		/*
+		 * method to add a shelving
+		 */
+		public void addShelving(Shelving shelving) {
+
+			sd.addShelving(shelving);
+		}// addShelving
+
+		/*
+		 * method to add a copy shelving
+		 */
+		public void addCopyShelving(int idShelving, int idCopy) {
+
+			sd.addCopyShelving(idShelving, idCopy);
+		}// addCopyShelving
+
+		/*
+		 * method to get a shelving by id
+		 */
+		public Shelving getShelvingById(int id) {
+			Shelving shelving = sd.getShelvingById(id);
+			return shelving;
+		}// getShelvingById
+
 	
 }//class ShelvingController
